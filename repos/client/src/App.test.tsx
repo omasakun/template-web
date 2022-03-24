829ce59fd579272f5946c17de9e0ad123ee14494
o137 <git-user@o137.net>
2022-03-25 00:18:35 +0900

    chore: add vitest and testing-library, add `@/*` to path

---
+++ repos/client/src/App.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import { describe, expect, test } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import App from '@/App'
+
+describe('App', () => {
+  test('the title is visible', () => {
+    render(<App />)
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
+  })
+
+  test('should increment count on click', async () => {
+    const user = userEvent.setup()
+    render(<App />)
+    await user.click(screen.getByRole('button'))
+    expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument()
+  })
+})
--- repos/client/vite.config.ts
+++ repos/client/vite.config.ts
@@ -1,8 +1,22 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import linaria from 'vite-plugin-linaria'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { InlineConfig } from 'vitest'
+
+// https://vitest.dev/config/
+const test: InlineConfig = {
+  globals: true, // otherwise, `@testing-library/jest-dom` won't work
+  environment: 'jsdom',
+  setupFiles: 'test/setup.ts',
+  includeSource: ['src/**/*.{js,ts}'],
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), linaria()],
+  plugins: [react(), linaria(), tsconfigPaths()],
+  define: {
+    'import.meta.vitest': 'undefined', // https://vitest.dev/guide/in-source.html#production-build
+  },
+  test,
 })
